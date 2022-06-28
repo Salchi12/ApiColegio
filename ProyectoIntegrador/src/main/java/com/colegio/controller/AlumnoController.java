dcad824fc589c1ddabffe4e0d3ae36c86f2ea9c8
SthivenParedes <jparedesb@unitru.edu.pe>
2022-06-27 23:19:23 -0500

    Modificacion del eliminar

--- ProyectoIntegrador/src/main/java/com/colegio/controller/AlumnoController.java
+++ ProyectoIntegrador/src/main/java/com/colegio/controller/AlumnoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,13 @@ public class AlumnoController {
 		final Alumno updateAlumno = alumnodao.save(alumno);
 		return ResponseEntity.ok(updateAlumno);
 		
+	}
+	
+	@DeleteMapping("eleminar/{id}")
+	public ResponseEntity<String> eliminarCliente(@PathVariable("id") Integer id) {
+		alumnodao.deleteById(id);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body("Alumno eliminado con Exito");
 	}
 	
 	
