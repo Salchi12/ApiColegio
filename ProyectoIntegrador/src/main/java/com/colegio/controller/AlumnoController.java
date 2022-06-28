a80a19b56bca3e3f5bbf77c2cb8f97f502c8e136
SthivenParedes <jparedesb@unitru.edu.pe>
2022-06-27 23:06:58 -0500

    Modificacion para hacer deploy

--- ProyectoIntegrador/src/main/java/com/colegio/controller/AlumnoController.java
+++ ProyectoIntegrador/src/main/java/com/colegio/controller/AlumnoController.java
@@ -1,13 +1,16 @@
 package com.colegio.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +32,38 @@ public class AlumnoController {
 		
 	}
 	
+	@GetMapping("/{id}")
+	public Optional<Alumno> obtenerAlumnoId(@PathVariable("id") Integer id) {
+		return alumnodao.findById(id);
+	}
+	
 	@PostMapping("/guardar")
 	public ResponseEntity<String> guardarCliente(@RequestBody Alumno alumno) {
 		alumnodao.save(alumno);
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body("Alumno guardado con Exito");
 	}
+	
+	@PutMapping("/editar/{id}")
+	public ResponseEntity<Alumno> editarAlumno(@PathVariable(value="id") Integer alumnoId, @RequestBody Alumno alumno) throws Exception{
+		Alumno alumnoById = alumnodao.findById(alumnoId)
+				.orElseThrow(() -> new Exception("Alumno no encontrado"));
+		
+		alumnoById.setNombre(alumno.getNombre());
+		alumnoById.setApellido(alumno.getApellido());
+		alumnoById.setDni(alumno.getDni());
+		alumnoById.setDireccion(alumno.getDireccion());
+		alumnoById.setFecha_nacimiento(alumno.getFecha_nacimiento());
+		alumnoById.setMorosos(alumno.getMorosos());
+		
+		final Alumno updateAlumno = alumnodao.save(alumno);
+		return ResponseEntity.ok(updateAlumno);
+		
+	}
+	
+	
+	
+	
+	
 
 }
--- ProyectoIntegrador/src/main/java/com/colegio/model/Alumno.java
+++ ProyectoIntegrador/src/main/java/com/colegio/model/Alumno.java
@@ -26,7 +26,7 @@ public class Alumno {
 	
 	@Id
 	@Column
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idalumnos;
 	
 	@Column
